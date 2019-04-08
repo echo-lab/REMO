7a06ac03187dc5517a225f3d22bdd99dceb648e4
Edward Powell <edwardp9@vt.edu>
2019-04-08 04:49:29 -0400

    Added a download feature

--- Basic Recording/main/scripts/background/main_panel.js
+++ Basic Recording/main/scripts/background/main_panel.js
@@ -51,6 +51,46 @@ clearButton.onclick = function () {
 };
 
 
+// Save stuff
+
+
+
+var saveButton = document.getElementById('save');
+saveButton.onclick = function () {
+
+
+    saveTextAsFile();
+
+};
+
+function destroyClickedElement(event) {
+    // remove the link from the DOM
+    document.body.removeChild(event.target);
+}
+
+function saveTextAsFile() {
+    var textToWrite = document.getElementById('box').innerText;
+    var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
+    var fileNameToSaveAs = "metadata.txt";
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+        // Firefox requires the link to be added to the DOM before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
+
+
 
 
 
